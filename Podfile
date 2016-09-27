503425a43e0691fd24b7dccb56e2f5c14acf4729
ronatory <ronatory@gmail.com>
2016-09-27 11:19:36 +0200

    Make ball movable via gesture

--- Podfile
+++ Podfile
@@ -1,13 +1,13 @@
 # Uncomment this line to define a global platform for your project
 # platform :ios, '9.0'
+use_frameworks!
 
 target 'colorBall' do
   # Comment this line if you're not using Swift and don't want to use dynamic frameworks
-  use_frameworks!
 
   # Pods for colorBall
-  pod 'RxSwift'
-  pod 'RxCocoa'
+  pod 'RxSwift',    '~> 2.0'
+  pod 'RxCocoa',    '~> 2.0'
   pod 'ChameleonFramework/Swift'
 
 end
--- colorBall/ViewController.swift
+++ colorBall/ViewController.swift
@@ -6,20 +6,40 @@
 //  Copyright © 2016 ronatory. All rights reserved.
 //
 
+import ChameleonFramework
 import UIKit
+import RxSwift
+import RxCocoa
+
 
 class ViewController: UIViewController {
 
+	var circleView: UIView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		setup()
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	func setup() {
+		// add circle
+		circleView = UIView(frame: CGRect(origin: view.center, size: CGSize(width: 100.0, height: 100.0)))
+		circleView.layer.cornerRadius = circleView.frame.width / 2.0
+		circleView.center = view.center
+		circleView.backgroundColor = UIColor.blueColor()
+		view.addSubview(circleView)
+		
+		// add gesture recognizer
+		let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(ViewController.circleMoved(_:)))
+		circleView.addGestureRecognizer(gestureRecognizer)
 	}
-
+	
+	func circleMoved(recognizer: UIPanGestureRecognizer) {
+		let location = recognizer.locationInView(view)
+		UIView.animateWithDuration(0.1) {
+			self.circleView.center = location
+		}
+	}
 
 }
 
